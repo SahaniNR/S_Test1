18c1ea6ef33fc7c9f0c5bd84a4b10f136d1a9618
SRANGANI <SRANGANI@CMLSRANGANI.Virtusa.com>
2018-03-14 17:34:05 -0500

    Second commit

--- Test_Git1/src/test/java/Test1/TestCase1.java
+++ Test_Git1/src/test/java/Test1/TestCase1.java
@@ -1,5 +1,39 @@
 package Test1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
 public class TestCase1 {
+	
+	protected WebDriver driver;
+	  @Test
+	  public void VerifyGuruTitle() throws InterruptedException {
+		    System.out.println("Logging to System");
+		  	ChromeOptions chromeOptions= new ChromeOptions();
+			chromeOptions.setBinary("C:\\Users\\srangani\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\srangani\\ChromeDriver\\chromedriver.exe");
+			String expecedTitle ="Guru99 Bank Home Page";
+			String actualTitle="";
+			ChromeDriver driver = new ChromeDriver(chromeOptions);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get("http://demo.guru99.com/");
+			driver.manage().window().maximize();
+			System.out.println("Guru99 page loaded sucessfully");
+			actualTitle=driver.getTitle();
+			if (actualTitle.contentEquals(expecedTitle))
+			{
+				System.out.println("Test Passed");
+			}
+			else
+			{
+				System.out.println("Test Failed");
+			}
+			System.out.println("Page Title is" + actualTitle);
+			driver.close();
+	  }
 
 }
